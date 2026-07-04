e4721b10afba23cf8c9c59e3efad1b4453c50582
Musiquammation <bryanmegherbi@gmail.com>
2026-07-04 11:41:53 +0200

    Rework InputHandler: - rework Control - add mouse buttons

--- client/handler/Control.ts
+++ client/handler/Control.ts
@@ -1,1 +1,11 @@
-export type Control = 'left' | 'right' | 'up' | 'down';
+export const CONTROLS = [
+	'left',
+	'right',
+	'up',
+	'down',
+	'mouse-left',
+	'mouse-middle',
+	'mouse-right'
+] as const;
+
+export type Control = typeof CONTROLS[number];
